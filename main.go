d1dbf87cf92a0dd9b43d5ce633f390cfe735f279
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-11-14 18:16:46 +0000

    Draw ninja spreadsheet

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	PlayerImage *ebiten.Image
+}
 
 func (g *Game) Update() error {
 	return nil
@@ -18,7 +20,8 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{120, 180, 255, 255})
 
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// draw player
+	screen.DrawImage(g.PlayerImage, &ebiten.DrawImageOptions{})
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -29,7 +32,12 @@ func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	if err := ebiten.RunGame(&Game{}); err != nil {
+
+	playerImg, _, err := ebitenutil.NewImageFromFile("assets/images/ninja.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ebiten.RunGame(&Game{PlayerImage: playerImg}); err != nil {
 		log.Fatal(err)
 	}
 }
